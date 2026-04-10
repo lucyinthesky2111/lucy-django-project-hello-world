63f43df073400a0dc07cb33582ac2369ad0f199c
Lucy Kidner <lucy.kidner@outlook.com>
2026-04-10 08:43:18 +0000

    Add necessary code and files to prepare project for deployment on Heroku

--- my_project/settings.py
+++ my_project/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-0chpry1(=p#kfivwrcsd(#ef*-zm^l!12h9f^y@z)%@8vj)ll2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.herokuapp.com', 'localhost']
 
 
 # Application definition
